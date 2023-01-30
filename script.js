ca8427df6ce240a13ffe78a413f80471f0f62f5d
confirmjs <confirmjss@gmail.com>
2023-01-30 16:46:28 +0600

    hoji

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const plusI = document.querySelector('.plus')
 const boxes = document.querySelector('.boxes')
+const box = document.querySelector('.box')
 const input = document.querySelector('.input')
 const delIcon = document.querySelector('.delIcon')
 const changeIcon = document.querySelector('.changeIcon')
@@ -8,17 +9,31 @@ const date = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Dat
 const hour = `${new Date().getHours()} : ${new Date().getMinutes()}`
 const allCount = document.querySelector('.all')
 const completedCount = document.querySelector('.completed')
-const box = document.querySelector('.box')
 
 
+// window.addEventListener('DOMContentLoaded', () => {
+//     const todoData = JSON.parse(localStorage.getItem('task'))
+//     const completedTodos = todoData.filter(item => item.completed)
+    
+
+//     allCount.innerText = `All : ${todoData.length}`
+//     completedCount.innerText = `Completed: ${completedTodos.length}`
+// })
 window.addEventListener('DOMContentLoaded', () => {
+    find()
+})
+
+
+
+function find () {
     const todoData = JSON.parse(localStorage.getItem('task'))
     const completedTodos = todoData.filter(item => item.completed)
-
+    
 
     allCount.innerText = `All : ${todoData.length}`
     completedCount.innerText = `Completed: ${completedTodos.length}`
-})
+}
+
 
 if(!localStorage.getItem('task')) {
     localStorage.setItem('task', JSON.stringify([]))
@@ -46,10 +61,8 @@ plusI.addEventListener('click', (e) => {
     localStorage.setItem('task', JSON.stringify(todo))
     boxes.insertAdjacentHTML('afterbegin', temp(input.value, date, hour))
     input.value = ''
+    find()
 })
-
-
-
 
 function temp (title, dateP, hourP, id, isCompleted) {
     return `
@@ -79,7 +92,6 @@ function deleteTodo(id) {
         window.location.reload()
     }
 }
-
 
 
 function editTodo(id) {
